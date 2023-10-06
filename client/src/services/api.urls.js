420b71fbce03bc0315194beb54c6353b5df02e72
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-10-06 21:38:00 +0530

    Updated api urls for navlinks

--- client/src/services/api.urls.js
+++ client/src/services/api.urls.js
@@ -2,5 +2,9 @@ export const API_URLS = {
     saveSentEmail:{
         endpoint:"save",
         method:"POST"
+    },
+    getEmailsFromType:{
+        endpoint:'emails',
+        method:"GET"
     }
 }
