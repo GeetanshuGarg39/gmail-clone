d1681f892a6b058ffe2e0af55ef2445d66e9150b
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-10-06 21:40:04 +0530

    Integerated APIs for Navlinks routes

--- client/src/components/Emails.js
+++ client/src/components/Emails.js
@@ -1,8 +1,17 @@
-import React from 'react'
-import { useOutletContext } from 'react-router-dom'
+import React, { useEffect } from 'react';
+import { useOutletContext ,useParams} from 'react-router-dom';
+import { API_URLS } from '../services/api.urls';
+import useApi from '../hooks/useApi';
 
 const Emails = () => {
   const {openDrawer} = useOutletContext();
+  const {type} = useParams();
+  const getEmailsService = useApi(API_URLS.getEmailsFromType);
+
+  useEffect(() => {
+    getEmailsService.call({},type)
+  },[type])
+  
     return (
     <div style={openDrawer ? {marginLeft:'250px', width:'100%'}:{width:'100%'}}>
       Mails
