dde8bb37b634b20f3ff33555df6f6668c537b3b9
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-10-07 15:10:36 +0530

    Added delete emails functionality

--- server/controller/email-controller.js
+++ server/controller/email-controller.js
@@ -15,11 +15,24 @@ export const saveSentEmail = (request,response) => {
 export const getEmails = async(request,response) => {
     try{
         let emails;
-        if(request.params.type){
+        if(request.params.type === 'bin'){
+            emails = await Email.find({bin:true})
+        }
+        else{
             emails = await Email.find({type:request.params.type})
         }
 
         return response.status(200).json(emails);
+    }catch(error){
+        console.log(error);
+        response.status(500).json(error.message);
+    }
+}
+
+export const moveEmailsToBin = async(request,response) => {
+    try{
+        await Email.updateMany({_id:{ $in:request.body }},{$set:{bin:true,starred:false,type:''}})
+        return response.status(200).json("emails deleted successfully");
     }catch(error){
         console.log(error);
         response.status(500).json(error.message);
