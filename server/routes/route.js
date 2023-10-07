122d3479990c8a79c087c4d0859bdc35a71b7df4
Geetanshu Garg <geetanshugarg39@gmail.com>
2023-10-07 19:02:11 +0530

    Created endpoint for starred email

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,5 +1,5 @@
 import  express  from "express";
-import { saveSentEmail,getEmails,moveEmailsToBin } from "../controller/email-controller.js";
+import { saveSentEmail,getEmails,moveEmailsToBin,toggleStarredEmails } from "../controller/email-controller.js";
 
 
 const router = express.Router();
@@ -9,5 +9,6 @@ router.post("/save",saveSentEmail);
 router.get("/emails/:type",getEmails);
 router.post('/save-draft',saveSentEmail);
 router.post('/bin',moveEmailsToBin);
+router.post('/starred',toggleStarredEmails);
 
 export default router;
